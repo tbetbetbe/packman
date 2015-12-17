aa3c5b85de866e127c0258e57770b24d69cba03d
Tim Emiola <temiola@google.com>
2015-12-16 16:01:57 -0800

    Update the python setup.py
    - Include the README.rst
    - Take a dependency on the common protos
    - Update the metadata

--- test/fixtures/setup.py
+++ test/fixtures/setup.py
@@ -11,7 +11,8 @@ from setuptools import setup, find_packages
 
 install_requires = [
   'oauth2client>=0.4.1',
-  'grpcio>=0.9.0a0'
+  'grpcio>=0.9.0a0',
+  'googleapis-common-protos>=3.0.0b1.1'
 ]
 
 setuptools.setup(
@@ -22,9 +23,15 @@ setuptools.setup(
   author_email='googleapis-packages@google.com',
   classifiers=[
     'Intended Audience :: Developers',
-    'Programming Language :: Python :: 2.7'
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: Apache Software License',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: Implementation :: CPython',
   ],
   description='GRPC library for service packager-unittest-v2',
+  long_description=open('README.rst').read(),
   install_requires=install_requires,
   license='Apache',
   packages=find_packages(),
