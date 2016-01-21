34f219ab236122e98568b9136783e76f0d05dc19
Tim Emiola <temiola@google.com>
2016-01-20 16:34:42 -0800

    Add python package configuration
    Also
    - add unittests for config.js

--- test/config.js
+++ test/config.js
@@ -14,52 +14,50 @@ function addFakeConfigFile(name, someYaml) {
   return configFile;
 }
 
-describe('config', function() {
-  var fakeYaml = "what:\n" +
-      "  - is\n" +
-      "  - this\n";
+var fakeYaml = "what:\n" +
+    "  - is\n" +
+    "  - this\n";
 
-  describe('method `packageInfo`', function() {
-    it('has a value by default', function() {
-      expect(config.packageInfo()).to.be.ok;
-    });
-
-    it('can load values via a config file', function() {
-      var opts = {
-        depsFile: addFakeConfigFile('deps', fakeYaml),
-        apiDefaultsFile: addFakeConfigFile('apiDefaults', fakeYaml)
-      };
-      expect(config.packageInfo(opts)).to.be.ok;
-      expect(config.packageInfo(opts).api.what).to.eql(['is', 'this']);
-      expect(config.packageInfo(opts).dependencies.what).to.eql(['is', 'this']);
-    });
+describe('method `packageInfo`', function() {
+  describe('has a value by default', function() {
+    expect(config.packageInfo()).to.be.ok;
   });
 
-  describe('method `commonPbPkgs`', function() {
-    it('has a value by default', function() {
-      expect(config.commonPbPkgs()).to.be.ok;
-    });
+  describe('can load values via a config file', function() {
+    var opts = {
+      depsFile: addFakeConfigFile('deps', fakeYaml),
+      apiDefaultsFile: addFakeConfigFile('apiDefaults', fakeYaml)
+    };
+    expect(config.packageInfo(opts)).to.be.ok;
+    expect(config.packageInfo(opts).api.what).to.eql(['is', 'this']);
+    expect(config.packageInfo(opts).dependencies.what).to.eql(['is', 'this']);
+  });
+});
 
-    it('can load values via a config file', function() {
-      var opts = {
-        commonPbFile: addFakeConfigFile('commonPb', fakeYaml)
-      };
-      expect(config.commonPbPkgs(opts)).to.be.ok;
-      expect(config.commonPbPkgs(opts).what).to.eql(['is', 'this']);
-    });
+describe('method `commonPbPkgs`', function() {
+  describe('has a value by default', function() {
+    expect(config.commonPbPkgs()).to.be.ok;
   });
 
-  describe('method `pythonPkg`', function() {
-    it('has a value by default', function() {
-      expect(config.pythonPkg()).to.be.ok;
-    });
+  describe('can load values via a config file', function() {
+    var opts = {
+      commonPbFile: addFakeConfigFile('commonPb', fakeYaml)
+    };
+    expect(config.commonPbPkgs(opts)).to.be.ok;
+    expect(config.commonPbPkgs(opts).what).to.eql(['is', 'this']);
+  });
+});
 
-    it('can load values via a config file', function() {
-      var opts = {
-        pythonPkgFile: addFakeConfigFile('pythonPkg', fakeYaml)
-      };
-      expect(config.pythonPkg(opts)).to.be.ok;
-      expect(config.pythonPkg(opts).what).to.eql(['is', 'this']);
-    });
+describe('method `pythonPkg`', function() {
+  describe('has a value by default', function() {
+    expect(config.pythonPkg()).to.be.ok;
+  });
+
+  describe('can load values via a config file', function() {
+    var opts = {
+      pythonPkgFile: addFakeConfigFile('pythonPkg', fakeYaml)
+    };
+    expect(config.pythonPkg(opts)).to.be.ok;
+    expect(config.pythonPkg(opts).what).to.eql(['is', 'this']);
   });
 });
